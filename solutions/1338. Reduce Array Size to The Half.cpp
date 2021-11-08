479fe4dc27dcbdfe0b4ec5e0130dd39a36082712
hx15 <52545789+hx15@users.noreply.github.com>
2021-11-09 00:25:17 +0200

    160 ms

--- solutions/1338. Reduce Array Size to The Half.cpp
+++ solutions/1338. Reduce Array Size to The Half.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         unordered_map<int, int> cnt;
+        vector<int> freqs;
         for (int x : arr) ++cnt[x];
+        for(const auto [_, freq] : cnt) freqs.push_back(freq);
+        sort(freqs.begin(), freqs.end(), [](int& l, int& r){
+            return l < r;
+        });
         
-        vector<int> frequencies;
-        for (auto [_, freq] : cnt) frequencies.push_back(freq);
-        sort(frequencies.begin(), frequencies.end());
+        int rem = arr.size(), half = rem/2, i = freqs.size()-1, count = 0;
+        while(rem > half)
+            rem -= freqs[i--], count++;
         
-        int ans = 0, removed = 0, half = arr.size() / 2, i = frequencies.size() - 1;
-        while (removed < half) {
-            ans += 1;
-            removed += frequencies[i--];
-        }
-        return ans;
+        
+        return count;
     }
 };
