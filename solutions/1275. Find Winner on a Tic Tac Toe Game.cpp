df707cfbdbbad0aae09f0900e7309c1891146b93
hx15 <52545789+hx15@users.noreply.github.com>
2021-11-14 23:55:13 +0200

    4 ms

--- solutions/1275. Find Winner on a Tic Tac Toe Game.cpp
+++ solutions/1275. Find Winner on a Tic Tac Toe Game.cpp
@@ -1,6 +1,51 @@
 class Solution {
 public:
     string tictactoe(vector<vector<int>>& moves) {
+        int r_size = moves.size();
+        int c_size = moves[0].size();
+        int n = 3;
+        vector<pair<int, int>> rows (n, {0,0});
+        vector<pair<int, int>> cols (n, {0,0});
+        vector<pair<int, int>> diags (2, {0,0});
         
+        for(int i = 0; i < moves.size(); i++){
+            if(!(i%2)){
+                rows[moves[i][0]].first++;
+                cols[moves[i][1]].first++;
+                diags[0].first += isMainDiagonal(n, moves[i][0], moves[i][1]) ? 1 : 0;
+                diags[1].first += isAntiDiagonal(n, moves[i][0], moves[i][1]) ? 1 : 0;
+                if(rows[moves[i][0]].first == n || cols[moves[i][1]].first == n || diags[0].first == n || diags[1].first == n )
+                    return "A";
+            }
+            else {
+                rows[moves[i][0]].second++;
+                cols[moves[i][1]].second++;
+                diags[0].second += isMainDiagonal(n, moves[i][0], moves[i][1]) ? 1 : 0;
+                diags[1].second += isAntiDiagonal(n, moves[i][0], moves[i][1]) ? 1 : 0;
+                if(rows[moves[i][0]].second == n || cols[moves[i][1]].second == n || diags[0].second == n || diags[1].second == n )
+                    return "B";
+            }
+        }
+        
+        for(int i = 0 ; i < rows.size() ; i++){
+            if(rows[i].first + rows[i].second != n)
+                return "Pending";
+        }
+        
+        return "Draw";
+        
+    }
+    
+    
+    bool isMainDiagonal(int n, int i, int j){
+        if(i == j)
+            return true;
+        return false;
+    }
+    
+    bool isAntiDiagonal(int n, int i, int j){
+        if(i + j == n-1)
+            return true;
+        return false;
     }
 };
