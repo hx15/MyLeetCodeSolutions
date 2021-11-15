9bdea3adaa715434c0985aef0ace6dfbba93aea7
hx15 <52545789+hx15@users.noreply.github.com>
2021-11-15 23:56:35 +0200

    200 ms

--- solutions/2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.cpp
+++ solutions/2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.cpp
@@ -11,6 +11,39 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        if(!head->next)
+            return {-1,-1};
+        // so we are treating them alike...
+        // the best way to solve this question, is to register localmin, and localmaxs throughout the array, at least their positions as we are iterating, and maybe //min distance could be any point in the localmin, and localmax that we collected, however for max we know it is the farthest one that we have registered.
+        vector<int> res (2, -1);
+        ListNode* pre = head; 
+        int i = 1;
+        int first_idx  = -1;
+        int last_cpoint = -1;
+        int minDistance = INT_MAX;
+        for(auto it = head->next ; it != NULL; it = it->next, i++, pre=pre->next ){
+            if(it->next == NULL)
+                continue;
+            if(isMaximaOrMinima(pre, it, it->next)) {
+                if(first_idx == -1) {first_idx = i, last_cpoint = i; continue;}
+                minDistance = min(minDistance, i - last_cpoint);
+                last_cpoint = i;
+            }
+        }
+        if(minDistance != INT_MAX)
+            res = {minDistance, last_cpoint - first_idx};
+            
+        return res;
         
+    }
+    
+    
+    
+    bool isMaximaOrMinima(ListNode*& pre, ListNode*& curr, ListNode*& next){
+        if(pre->val < curr->val && next->val < curr->val)
+            return true;
+        else if(pre->val > curr->val && next->val > curr->val)
+            return true;
+        return false;
     }
 };
